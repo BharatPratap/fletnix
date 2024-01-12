c45a0281f97342cca56e082393315342d3c5e733
BPratap <bp171020@gmail.com>
2024-01-12 23:13:02 +0530

    set angular app

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  template: `
+    Defauuult
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'defaulttt';
+}
