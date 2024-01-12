15ceccd8c18d62334bb5cc779bdb16c8ea9e5670
BPratap <bp171020@gmail.com>
2024-01-12 23:34:58 +0530

    set angular app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `
-    Defauuult
-  `,
-  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'defaulttt';
+  title = 'frontend';
 }
